640ca3403def76a1e4e4df3c603b203391dfd593
Little0witch <eeeee.nom@gmail.com>
2023-02-27 17:33:35 +0300

    First commit
    Our commit.

--- randomeNum/src/main/java/com/bsuir/labs/randomeNum/controller/ExController.java
+++ randomeNum/src/main/java/com/bsuir/labs/randomeNum/controller/ExController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExController {
 	@GetMapping("/hello")
 	public String Hello() {
-		return "Hello!";
+		return "Hello world!";
 	}
 
 }
