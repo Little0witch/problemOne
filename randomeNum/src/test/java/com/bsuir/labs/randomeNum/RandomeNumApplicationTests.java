06171ec79cea43c499bb9ec502aede1ea771b4d2
Little0witch <eeeee.nom@gmail.com>
2023-02-27 17:25:53 +0300

    First Commit

---
+++ randomeNum/src/test/java/com/bsuir/labs/randomeNum/RandomeNumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsuir.labs.randomeNum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomeNumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
